6dce64a44c764f03aee928c466d8e96c7ee58834
Apeksha Tarate <87703951+apeksha3626@users.noreply.github.com>
2022-04-10 10:02:43 +0530

    This is my first commit with original file.

---
+++ pattern.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+main(){
+	int i,j,n,k;
+	printf("Enter number of rows for pattern ::");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		
+		for(k=n-i;k>=1;k--){
+			printf("  ");
+		
+							}
+			for(j=1;j<=i;j++){
+				printf("%d",j);
+		
+								}
+		printf("\n");	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
